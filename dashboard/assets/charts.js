d884ba7f4796e1944b60f682aa85610d257ad127
lemon0n <lemon0n@github.com>
2026-08-10 23:37:05 +0000

    daily update 2026-08-11

--- dashboard/assets/charts.js
+++ dashboard/assets/charts.js
@@ -88,11 +88,11 @@
     animation:false,
     tooltip:{trigger:'axis',appendToBody:true},
     grid:{left:'3%',right:'4%',bottom:'10%',containLabel:true},
-    xAxis:{type:'category',data:["crowding", "withdrawal_risk", "share_flow_signal", "acceleration", "news_price_gap", "sentiment_score", "bullish_count", "bearish_count", "prev_change_pct", "prev_volume_ratio", "prev_intraday_return", "sector_mentioned", "sector_mention_count", "hs300_mom_5d", "vol_10d", "retail_sentiment", "rzjme_yi", "sentiment_divergence", "behavior_momentum", "flow_proxy", "early_entry", "news_surprise", "market_breadth", "external_signal", "external_news_count", "news_flow_gap"],axisLabel:{color:muted,fontSize:9,rotate:25},axisLine:{lineStyle:{color:rule}}},
+    xAxis:{type:'category',data:["crowding", "withdrawal_risk", "share_flow_signal", "vol_10d", "acceleration", "sentiment_score", "bullish_count", "bearish_count", "prev_change_pct", "prev_volume_ratio", "prev_intraday_return", "sector_mentioned", "sector_mention_count", "hs300_mom_5d", "retail_sentiment", "rzjme_yi", "sentiment_divergence", "behavior_momentum", "flow_proxy", "early_entry", "news_surprise", "market_breadth", "external_signal", "external_news_count", "news_price_gap", "news_flow_gap"],axisLabel:{color:muted,fontSize:9,rotate:25},axisLine:{lineStyle:{color:rule}}},
     yAxis:{type:'value',axisLabel:{color:muted},splitLine:{lineStyle:{color:rule}}},
     series:[{
       type:'bar',
-      data:[0.038854, 0.03075, 0.027656, 0.002895, 0.000414, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+      data:[0.039619, 0.028844, 0.026705, 0.002877, 0.001558, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
       itemStyle:{color:gold},
       barWidth:'45%',
       label:{show:true,position:'top',formatter:'{c}',color:muted,fontSize:9}
